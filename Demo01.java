c2c21b6ae2bd49f45c5a5c44aeae2d8d5aef6ce9
misalnitin1504 <113243105+misalnitin1504@users.noreply.github.com>
2024-03-25 23:31:04 +0530

    New Update Demo01.java

--- Demo01.java
+++ Demo01.java
@@ -2,6 +2,6 @@ package com.prominentpixel.git.DemoRepo;
 
 public class Demo01 {
     public static void main(String[] args) {
-        System.out.println("Hello Git....");
+        System.out.println("Hello Git.....");
     }
 }
